b199d751baa56f9dbe9876c4de1f9f7582d2433f
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-05-07 23:35:13 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,7 +4,7 @@ async function handler(m) {
                     + 'VERSION:3.0\n' 
                     + 'N:;frmndaa;;;'
                     + 'FN:frmndaa\n' // full name
-                    + 'ORG:Ilman;\n' // the organization of the contact
+                    + 'ORG:frmndaa;\n' // the organization of the contact
                     + 'TEL;type=CELL;type=VOICE;waid=6282365089294:+62 823-6508-9294\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'frmndaa', contacts: [{ vcard }] } }, { quoted: m })
