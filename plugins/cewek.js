ca6fb8f5c06e2f74219e2ed91d3979363dccd858
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-05-12 15:47:07 +0700

    Create cewek.js

---
+++ plugins/cewek.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn }) => {
+ await conn.sendFile(m.chat, global.API('xteam', '/randomimage/cewe', { text: © SHIRAORI }, ,APIKEY'), 'cewek.jpg', teks, m)
+}
+handler.help = ['cewek']
+handler.tags = ['random']
+handler.command = /^(cewek)$/i
+
+  handler.limit = true
+  
+  module.exports = handler
